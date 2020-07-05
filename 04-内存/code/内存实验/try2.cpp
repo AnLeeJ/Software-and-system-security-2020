9f3663e1930cea51887881a3728a3553bee7e980
AnLeeJ <986727821@qq.com>
2020-07-05 21:19:33 +0800

    finish

---
+++ 04-内存/code/内存实验/try2.cpp
@@ -0,0 +1,17 @@
+//#include<iostream>
+//#include<windows.h>
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<time.h>
+//using namespace std;
+//int main() {
+//	LPVOID pv;
+//	pv = VirtualAlloc(NULL, 1000 * 1024 * 1024, MEM_RESERVE | MEM_TOP_DOWN, PAGE_READWRITE);
+//	if (pv == NULL)
+//		cout << "无法提供大量虚拟空间！\n" << endl;
+//	MEMORYSTATUS memStatusVirtual1;
+//	GlobalMemoryStatus(&memStatusVirtual1);
+//	cout << "虚拟内存分配：" << endl;
+//	printf("指针地址=%x\n",pv);
+//	return 0;
+//}
